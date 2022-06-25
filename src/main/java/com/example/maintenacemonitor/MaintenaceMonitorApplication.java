106ed8094ebf6a64ff65277969c1d8443d56d774
wi21b025 <wi21b025@technikum-wien.at>
2022-06-25 19:13:56 +0200

    Initial commit

---
+++ src/main/java/com/example/maintenacemonitor/MaintenaceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.maintenacemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaintenaceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaintenaceMonitorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/maintenacemonitor/MaintenaceMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maintenacemonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaintenaceMonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
